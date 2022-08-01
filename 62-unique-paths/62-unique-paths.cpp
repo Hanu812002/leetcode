fa6a65d7149c4390ffb1b57027bc237777ef0f08
Hanu812002 <87744370+Hanu812002@users.noreply.github.com>
2022-08-01 18:33:44 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -12,5 +12,17 @@ public:
         vector<vector<int>> dp(m , vector<int>(n , 0));
         return solve(0, 0, dp, m, n);
     }
+    //  int i,j,k,l,m,n;
+    //     int dp[r+1][c+1]; // dp array, dp[i][j] stores number of ways to reach the pint (i,j) in 0 based indexing.
+    //     for(i=0;i<r;i++)
+    //     {
+    //         for(j=0;j<c;j++)
+    //         {
+    //             if(i==0 or j==0)dp[i][j]=1; //since we can move either right or down only 
+    //             else dp[i][j]=dp[i][j-1]+dp[i-1][j]; // we can reach a position from up and left of that point, thus add them
+    //         }
+    //     }
+    //     return dp[r-1][c-1];
+    // }
     
 };
