697588aa7466c56bfdd420825991a1e2f8247453
Hanu812002 <87744370+Hanu812002@users.noreply.github.com>
2022-08-23 01:12:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        
         if(n==1)
             return true;
         if(n==0)
@@ -9,10 +8,5 @@ public:
         if(n%4!=0)
             return false;
        return isPowerOfFour(n/4);
-        // return true;
-        //  def isPowerOfThree(self, n: int) -> bool:
-        // while(n%3==0 and n>0):
-        //     n = n/3
-        // return n==1
     }
 };
