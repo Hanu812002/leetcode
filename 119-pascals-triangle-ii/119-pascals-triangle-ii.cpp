637fbd9c8983ffeab81a077ace756db5224dce47
Hanu812002 <87744370+Hanu812002@users.noreply.github.com>
2022-09-07 23:42:40 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (99.13%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getRow(int r) {
+        vector<int>v(r+1,0);
+        v[0]=1;
+        for(int i=1;i<=r;i++){
+            for(int j=i;j>=1;j--)
+                v[j]+=v[j-1];
+        }
+        return v;
+    }
+};
