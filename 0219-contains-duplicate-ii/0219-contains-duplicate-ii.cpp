d12821e898d7a74902abb9fad0b6ad84feffe64c
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2022-10-21 23:21:01 +0530

    Time: 336 ms (61.07%), Space: 73 MB (83.64%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       vector<pair<int,int>>v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        for(int i=1;i<v.size();i++){
+            if(v[i-1].first==v[i].first)
+            {
+                if(abs(v[i-1].second-v[i].second)<=k)
+                    return true;
+            }
+        }
+        return false;  
+    }
+};
