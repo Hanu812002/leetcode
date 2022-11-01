b149df1eabb43f7af952b2360a4474a30cf0bc0c
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2022-11-01 23:11:06 +0530

    Time: 51 ms (40.41%), Space: 13 MB (96.41%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int>adj[n];
+	       
+        int m=pre.size();
+        for(int i=0;i<m;i++){
+    int a=pre[i][0];
+    int b=pre[i][1];
+    adj[b].push_back(a);
+        }
+        vector<int>ans;
+      vector<int>indegree(n,0);
+      for(int i=0;i<n;i++){
+          for(auto x:adj[i]){
+              indegree[x]++;
+          }
+      }
+        queue<int>q;
+     
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+    int count=0;
+    while(!q.empty()){
+        
+        int temp=q.front();
+        q.pop();
+        count++;
+       ans.push_back(temp);
+        for(auto x:adj[temp]){
+            indegree[x]--;
+            if(indegree[x]==0){
+                q.push(x);
+            }
+        }
+        
+        
+    }
+    if(count==n)return true;
+    return false;
+    }
+};
