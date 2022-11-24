6cb5f41bc9e60ede0ce78b005ac106a9cfe96c46
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2022-11-24 14:03:40 +0530

    Time: 188 ms (83.57%), Space: 85.3 MB (54.01%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>v;
+    void in(TreeNode* r){
+        if(r==NULL)
+            return;
+        in(r->left);
+        v.push_back(r->val);
+        in(r->right);
+    }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        in(root1);
+        in(root2);
+        
+        sort(v.begin(),v.end());
+        return v;
+        
+    }
+};
