64943d4643a11adda112e9777aaa22eb8e3d98ec
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-01-05 15:11:29 +0530

    Time: 20 ms (36.33%), Space: 10.8 MB (61.73%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> ans; // 2 D vector to store our answer
-    void solve(int i, vector<int>& arr, vector<int>& temp, int target)
+    vector<vector<int>> ans; 
+  // set<vector<int>>s;
+    void solve(int in, vector<int>& arr, vector<int>& temp, int target)
     {
         if(target == 0) 
         {
             ans.push_back(temp); 
             return; 
         }
+        
+        for(int i=in;i<arr.size();i++){
+        
         if(target < 0)
             return;
-        if(i == arr.size())
-            return;
-        solve(i + 1, arr, temp, target);
+        
+         
         temp.push_back(arr[i]); 
         solve(i, arr, temp, target - arr[i]); 
         temp.pop_back();
         
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
         ans.clear();
