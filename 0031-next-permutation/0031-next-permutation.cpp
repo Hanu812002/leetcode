2694624100636bf9a506a729f76e096862be6f3f
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-01-29 00:49:50 +0530

    Time: 3 ms (86.48%), Space: 12.2 MB (39.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-            return ;
+    void nextPermutation(vector<int>& arr) {
+       // code here
+        int n=arr.size();
         int i=n-2;
-        while(i>=0 && nums[i]>=nums[i+1]){
+        while(i>=0){
+            if(arr[i]<arr[i+1])
+             break;
+             
             i--;
         }
-        if(i>=0){
-            int j=n-1;
-            while(j>=0 && nums[i]>=nums[j])
-                j--;
-            swap(nums[i],nums[j]);
+        if(i<0)
+        {
+            reverse(arr.begin(),arr.end());
+            // return arr;/
+            
         }
-        reverse(nums.begin()+i+1,nums.end());
+        else{
+       int j=n-1;
+       while(i>=0 && arr[j]<=arr[i] && j>i)
+        j--;
         
+        // cout<<i<<" "<<j<<endl;
+      swap(arr[i],arr[j]);
+      reverse(arr.begin()+i+1,arr.begin()+n);
+       // return arr;
+        }
     }
 };
