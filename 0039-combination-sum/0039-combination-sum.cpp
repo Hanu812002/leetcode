777b74da2409a552e6df4b9031caefca31eb73c5
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-02-02 23:13:33 +0530

    Time: 48 ms (25.85%), Space: 48.3 MB (14.16%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> ans; 
-  // set<vector<int>>s;
-    void solve(int i, vector<int>& arr, vector<int>& temp, int target)
-    {
-        if(target == 0) 
-        {
-            ans.push_back(temp); 
-            return; 
-        }
-        
-        if(i==arr.size())
-            return;
-        
-        if(target < 0)
-            return;
-        
-        solve(i+1,arr,temp,target);
-        
-        temp.push_back(arr[i]); 
-        solve(i, arr, temp, target - arr[i]); 
-        temp.pop_back();
-        
-        
-    }
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        ans.clear();
-        vector<int> temp; 
-        solve(0, arr, temp, target);
-        return ans; 
+      vector<vector<int>>ans;
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    	void fun(vector<int>arr,int i,int n,int sum,vector<int>&v){
+	   
+	        
+	    if(i==n){
+	         if(sum==0)
+	        ans.push_back(v);
+	       return;
+	    }
+	   // if(sum</
+	      
+	    if(sum>=arr[i]){
+	    v.push_back(arr[i]);
+	    fun(arr,i,n,sum-arr[i],v);
+	    v.pop_back();
+	    }
+	    fun(arr,i+1,n,sum,v);
+	      
+	}
+
+    vector<vector<int>> combinationSum(vector<int>& c, int target) {
+     vector<int>v;
+        fun(c,0,c.size(),target,v);
+        return ans;
     }
 };
