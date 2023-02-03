238eb1b232e5a61f667b8a4aa204c32a02862d73
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-02-04 00:55:05 +0530

    Time: 41 ms (91.52%), Space: 35.5 MB (34.77%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+     bool dfs(vector<int>& arr , int cur){
+        if(cur >= arr.size() || cur < 0 || arr[cur] == -1) return false;
+        if(arr[cur] == 0){
+            return true;
+        }
+        int off = arr[cur];
+        arr[cur] = -1;
+        return dfs(arr , cur + off) || dfs(arr , cur - off);
+        
+    }
+    bool canReach(vector<int>& arr, int start) {
+                return dfs(arr , start);
+
+    }
+};
