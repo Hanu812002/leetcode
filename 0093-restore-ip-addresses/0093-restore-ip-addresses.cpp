04ac5c30acf141de1a43bee76b7250d2a68ac818
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-02-20 18:42:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (33.85%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool check(string s){
-        int n=s.size();
-        
-        if(n==1){
+        int n=s.length();
+        if(n==1)
             return true;
-        }
-        if(n>3||s[0]=='0'){
+        if(n>3 || s[0]=='0')
             return false;
-        }
-
+        
         int val=stoi(s);
         if(val>255){
             return false;
@@ -20,28 +17,27 @@ public:
     vector<string> restoreIpAddresses(string s) {
         int n=s.size();
         vector<string>ans;
-        if(n>12){
+        
+        if(n>12)
             return ans;
-        }
         
         for(int i=1;i<=3;i++){
             for(int j=1;j<=3;j++){
                 for(int k=1;k<=3;k++){
-                    if(i+j+k<n&&i+j+k+3>=n){
-                     
-                        string a=s.substr(0,i);
-                        string b=s.substr(i,j);
-                        string c=s.substr(j+i,k);
-                        string d=s.substr(i+j+k);
-                    
-                        if(check(a)&&check(b)&&check(c)&&check(d)){
-                            ans.push_back(a+"."+b+"."+c+"."+d);
-                        }
+                    if(i+j+k<n && i+j+k+3>=n){
+                         
+                     string a=s.substr(0,i);
+                     string b=s.substr(i,j);
+                     string c=s.substr(i+j,k);
+                     string d=s.substr(i+j+k);
+                        
+                    if(check(a) && check(b) && check(c) && check(d))
+                        ans.push_back(a+"."+b+"."+c+"."+d);
                     }
                 }
             }
         }
-    
+        
         return ans;
     }
 };
