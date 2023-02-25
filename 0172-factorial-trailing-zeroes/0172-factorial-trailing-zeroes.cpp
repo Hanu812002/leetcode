581d316ba5a2e2c17ed60bd8cceb9fbc8b58d3f8
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-02-25 12:31:43 +0530

    Time: 2 ms (51.58%), Space: 6.1 MB (11.71%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-           // Write Your Code here
-        // int n=N;
-        int c=0;
+         int c=0;
         while(n>0){
             c+=(n/5);
             n=n/5;
