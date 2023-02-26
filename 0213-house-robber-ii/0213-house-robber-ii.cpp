c516f9aa521a96f126e4febd9c00e19bea67d6ce
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-02-26 13:44:07 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (7.85%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
+        int dp[105];
+    int fun(vector<int>nums,int i,int j){
+        if(i>j)
+            return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        return dp[i]=max(fun(nums,i+1,j),nums[i]+fun(nums,i+2,j));
+    }
     int rob(vector<int>& nums) {
-      if (nums.size() == 1){
+        
+        if(nums.size()==1)
             return nums[0];
-        }
-        const int n = nums.size()+2;
-        int dp1[n];
-        dp1[n-1] = 0;
-        dp1[n-2] = 0;
-        for(int i=n-3; i>=1; i--){ //don't rob the first house
-            dp1[i] = max(nums[i]+ dp1[i+2],dp1[i+1]);
-        }
-        //if we rob the first house, we couldn't rob the last one
-        int dp2[n];
-        dp2[n-1] = 0;
-        dp2[n-2] = 0;
-        dp2[n-3] = 0;//we set up the profit of last house as 0
-
-        for(int i=n-4; i>=0; i--){ // rob the first house
-            dp2[i] = max(nums[i]+ dp2[i+2],dp2[i+1]);
-        }
-        return max(dp1[1], dp2[0]);   
+        
+        memset(dp, -1, sizeof(dp));
+        int a=fun(nums,1,nums.size()-1);
+        
+        memset(dp, -1, sizeof(dp));
+         int b=fun(nums,0,nums.size()-2);
+        
+        return max(a,b);
     }
 };
