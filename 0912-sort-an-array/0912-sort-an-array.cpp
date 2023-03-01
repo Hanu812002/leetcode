08176524cfb976b73d939eff7c40229f51efdb55
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-03-02 00:06:37 +0530

    Time: 1012 ms (11.45%), Space: 268.5 MB (5.51%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void MergeSortedIntervals(vector<int>& v, int s, int m, int e) {
+	
+    // temp is used to temporary store the vector obtained by merging
+    // elements from [s to m] and [m+1 to e] in v
+	vector<int> temp;
+
+	int i, j;
+	i = s;
+	j = m + 1;
+
+	while (i <= m && j <= e) {
+
+		if (v[i] <= v[j]) {
+			temp.push_back(v[i]);
+			++i;
+		}
+		else {
+			temp.push_back(v[j]);
+			++j;
+		}
+
+	}
+
+	while (i <= m) {
+		temp.push_back(v[i]);
+		++i;
+	}
+
+	while (j <= e) {
+		temp.push_back(v[j]);
+		++j;
+	}
+
+	for (int i = s; i <= e; ++i)
+		v[i] = temp[i - s];
+
+}
+
+void MergeSort(vector<int>& v, int s, int e) {
+	if (s < e) {
+		int m = (s + e) / 2;
+		MergeSort(v, s, m);
+		MergeSort(v, m + 1, e);
+		MergeSortedIntervals(v, s, m, e);
+	}
+}
+
+    vector<int> sortArray(vector<int>& arr) {
+
+        int n=arr.size();
+    	MergeSort(arr, 0, n - 1);
+        return arr;
+    }
+};
