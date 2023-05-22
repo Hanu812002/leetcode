67c484547f3f21de890953190e79457bdfbf54fc
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-05-22 23:49:54 +0530

    Time: 21 ms (50.72%), Space: 16.5 MB (96.81%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,25 +3,25 @@ public:
     int jump(vector<int>& nums) {
          int maxr=nums[0];
         int jump=nums[0];
-        int steps=1;
-        
         if(nums.size()==1)
             return 0;
         
+        if(maxr==0)
+            return -1;
+        int c=1;
         for(int i=1;i<nums.size();i++){
             if(i==nums.size()-1)
-                return steps;
+                return c;
             
             maxr=max(maxr,nums[i]+i);
             jump--;
             if(jump==0){
-                // if(maxr<=i)
-                //     return false;
+                c++;
+                if(maxr<=i)
+                    return -1;
                 jump=maxr-i;
-                steps++;
             }
         }
-        return steps;
+        return 0;
     }
-}; 
-  
+};
