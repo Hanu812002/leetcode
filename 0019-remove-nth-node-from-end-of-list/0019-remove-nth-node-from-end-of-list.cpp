a89fd18bd655b290c2fa5d950546a3fdd6f7009f
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-06-08 04:34:46 +0530

    Time: 5 ms (54.05%), Space: 10.6 MB (55.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -6,60 +6,34 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };*/
- 
-
- //  ListNode* removeNthFromEnd(ListNode* head, int n) {
- //        ListNode *fast = head, *slow = head;
- //        for (int i = 0; i < n; i++) fast = fast->next;
- //        if (!fast) return head->next;
- //        while (fast->next) fast = fast->next, slow = slow->next;
- //        slow->next = slow->next->next;
- //        return head;
- //    }
-
-
+ * };
+ */
 class Solution {
 public:
-    int length(ListNode* head){
-        int c=0;
-        
-        while(head!=NULL){
-            c++;
-            head=head->next;
-        }
-        return c;
-    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-       if(head==NULL)
-           return head;
+        ListNode* temp=head;
         
+        for(int i=0;i<n-1;i++)
+            temp=temp->next;
         
-        int len=length(head);
-        ListNode* r=head;
+        if(temp->next==NULL)
+            return head->next;
         
-        int d=len-n;
-        
-        if(d==0)
-        {
-            head=head->next;
-            return head;
+        ListNode* l=head;
+        ListNode* p=NULL;
+        while(temp->next){
+            temp=temp->next;
+            p=l;
+            l=l->next;
+            
         }
-        
-        for(int i=1;i<d;i++){
-            r=r->next;
-        }
-        
-        if(n==1){
-         r->next=NULL;   
-        }
-        else{
-        ListNode* l=r->next;
-        r->next=l->next;
-        delete(l);
-        }
-        
-        // cout<<len<<endl;
+                                    // cout<<p->val;
+      if(l->next==NULL)
+          p->next=NULL;
+      else{
+          p->next=l->next;
+          l->next=NULL;
+      }
         return head;
     }
 };
