57da43ca3600cdcd142a03816a4d56211ea06f2b
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-08-01 10:26:30 +0530

    Time: 391 ms (17.52%), Space: 164.9 MB (7.32%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -4,16 +4,16 @@ public:
        void fun(vector<int>a,int n, int k, int i){
            
            if(a.size()==k){
-               // cout<<a[0]<<" "<<a[1]<<endl;
                v.push_back(a);
-               
                return;
            }
+           
             for(int j=i;j<=n;j++){
                 a.push_back(j);
                 fun(a,n,k,j+1);
                 a.pop_back();
             }
+           
        }
     vector<vector<int>> combine(int n, int k) {
         vector<int>a;
