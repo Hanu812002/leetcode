6ae07efd9ee7cca97203aab4859e470a712b8202
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-08-07 12:26:47 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,64 +1,63 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//       int i=0;
-//       int j=matrix.size()-1;
-//       int col=matrix[0].size()-1;
-//         while(i<j){
-//             int m=i+(j-i)/2;
-            
-//              if(matrix[m][0] <= target && target<=matrix[m][col-1])
-//              { 
-//                 break;
-//              }
-//             if(matrix[m][0]>target){
-//                 j=m-1;
-//             }
-//             else if(matrix[m][col-1]<target )
-//             {
-//                 i=m+1;
-//             }
-            
-//         }
-//         // cout<<i<<" "<<j<<endl;
+    int find_row(vector<vector<int>>& matrix, int target, int m, int n){
         
-//         int x=0;
-//         while(x<col){
-//             int m=x+(col-x)/2;
-            
-//             if(matrix[i][m]==target)
-//                 return true;
-//             else if(matrix[i][m]>target)
-//                 col=m-1;
-//             else
-//                 x=m+1;
-//         }
         
-//         return false;
-        int row = matrix.size();
-        int col = matrix[0].size();
-
-        int start = 0;
-        int end = row*col - 1;
-
-        int mid = (start+end)/2;
-
-        while(start<=end){
-            int element = matrix[mid/col][mid%col];
-
-            if(element==target){
-                return 1;
-            }
-
-            if(element<target){
-                start = mid+1;
+        int i=0;
+        int j=m-1;
+        
+        while(i<=j){
+            
+            
+            int mid=i+(j-i)/2;
+            
+            cout<<i<<"  "<<j<<"  "<<mid<<endl;
+            
+            if(matrix[mid][0]<=target && matrix[mid][n-1]>=target)
+                return mid;
+            
+            else if(matrix[mid][0]>target){
+                j=mid-1;
             }
             else{
-                end = mid-1;
+                i=mid+1;
             }
-            mid = (start+end)/2;
         }
-        return 0;
-    
+        
+        
+        
+        return -1;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        int row=find_row(matrix,target,m,n);
+        
+        // cout<<row<<endl;
+        if(row==-1)
+          return false;
+        
+        int i=0;
+        int j=n-1;
+        
+         while(i<=j){
+            
+            
+            int mid=i+(j-i)/2;
+            
+            if(matrix[row][mid]==target)
+                return true;
+            
+            else if(matrix[row][mid]>target){
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+        }
+        
+        return false;
     }
 };
