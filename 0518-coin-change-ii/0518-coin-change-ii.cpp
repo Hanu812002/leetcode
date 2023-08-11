c1d09d9f978a5422019889ac75526502163ba540
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-08-11 23:27:29 +0530

    Time: 20 ms (63.36%), Space: 11.9 MB (75.37%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int change(int n, vector<int>& S) {
+       int m=S.size();
+        int tb[m+1][n+1];
+        
+        for(int i=0;i<=m;i++){
+            tb[i][0] = 1;
+        }
+        
+        for(int i=1;i<=n;i++){
+            tb[0][i] = 0;
+        }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(S[i-1]<=j){
+                    tb[i][j] = tb[i][j-S[i-1]]+tb[i-1][j];
+                }
+                
+                else{
+                    tb[i][j] = tb[i-1][j];
+                }
+                
+            }
+        }
+        
+        return tb[m][n];
+   
+    }
+};
