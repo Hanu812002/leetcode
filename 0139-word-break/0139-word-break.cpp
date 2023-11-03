0e56982fe37fca9a967b8abf59472de9c1875c55
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2023-11-03 10:06:37 +0530

    Time: 4 ms (78.12%), Space: 10.3 MB (54.36%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -5,28 +5,23 @@ public:
     bool fun(string s,int i){
         if(i>=s.length())
             return prev[i]=true;
-        
         if(prev.find(i)!=prev.end())
             return prev[i];
-        
         string str="";
-        
         for(int k=i;k<s.length();k++){
-            str+=s[k];
-            
+            str+=s[k];   
             if(mp.find(str)!=mp.end() && fun(s,k+1))
               return  prev[k]=true;
-            
-            // i++;
         }
-        
         return prev[i]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        
         for(int i=0;i<wordDict.size();i++)
             mp[wordDict[i]]++;
-        
         return fun(s,0);
     }
 };
+
+
+// Time complexity: ( O(n * m) ), where ( n ) is the length of the string and ( m ) is the maximum length of a word in the dictionary.
+// Space complexity: ( O(n) )
