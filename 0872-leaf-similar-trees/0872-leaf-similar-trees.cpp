846e2135767d4ef45cc5fd12e1eb488d1b6a0844
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2024-01-09 10:31:39 +0530

    Time: 6 ms (22.71%), Space: 13.4 MB (30.37%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -1,31 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-     vector<int> helper(TreeNode* root){
-        if(root==NULL) return {};
-        if(root->left==NULL && root->right==NULL) return {root->val};
-        vector<int> left=helper(root->left);
-        vector<int> right=helper(root->right);
-        vector<int> ans;
-        for(int  i=0;i<left.size();i++) ans.push_back(left[i]);
-        for(int  i=0;i<right.size();i++) ans.push_back(right[i]);
-        return ans;
-    }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans1=helper(root1);
-        vector<int> ans2=helper(root2);
-        if(ans1.size()!=ans2.size()) return false;
-        for(int i=0;i<ans1.size();i++) if(ans1[i]!=ans2[i]) return false;
-        return true;
+        vector<int> nodeList_root1, nodeList_root2;
+        findLeaf_dfs(root1, nodeList_root1);
+        findLeaf_dfs(root2, nodeList_root2);
+        return nodeList_root1 == nodeList_root2;
+    }
+
+    void findLeaf_dfs(TreeNode* nodeNow, std::vector<int>& nodeList) {
+        if (!nodeNow) {
+            return;
+        }
+
+        if (!nodeNow->left && !nodeNow->right) {
+            nodeList.push_back(nodeNow->val);
+            return;
+        }
+
+        findLeaf_dfs(nodeNow->left, nodeList);
+        findLeaf_dfs(nodeNow->right, nodeList);
     }
 };
