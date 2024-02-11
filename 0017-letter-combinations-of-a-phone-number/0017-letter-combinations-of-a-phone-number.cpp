6fe3e595b12996e3fb14f75829724fc5394bf28d
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2024-02-11 13:24:13 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (14.61%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-   void solve(vector<vector<char> >&v,vector<string>&ans,string digits,int pos,string s)
-    {
-       
-        if(pos==digits.size())
+    void fun(vector<string>&ans,string digit,int a,vector<vector<char>>&vec,string s){
+        
+        if(digit.length()==a)
         {
             if(s!="")
-            ans.push_back(s);return;
+                ans.push_back(s);
+            
+            return ;
         }
-       
-        int temp=digits[pos]-'0';
-       
-        for(int i=0;i<v[temp].size();i++)
-        {
-            s.push_back(v[temp][i]);
-            solve(v,ans,digits,pos+1,s);
+        
+        int t=digit[a]-'0';
+        
+        for(int i=0;i<vec[t].size();i++){
+            s.push_back(vec[t][i]);
+            fun(ans,digit,a+1,vec,s);
             s.pop_back();
         }
-       
+        
+        
+        
     }
     vector<string> letterCombinations(string digits) {
-        string s;
         
-        vector<vector<char> > v{{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},
-                               {'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
-        vector<string> ans;
-        solve(v,ans,digits,0,"");
+        vector<vector<char>>vec{{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+        
+        vector<string>ans;
+        
+        fun(ans,digits,0,vec,"");
+        
         return ans;
     }
 };
