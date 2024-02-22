a2e02513c64978338cd461b177eeea698a2401c2
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2024-02-22 09:33:50 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (30.25%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int l=0;
-        int t=0;
-        int r=matrix[0].size()-1;
-        int d=matrix.size()-1;
-        vector<int>v;
+       vector<int>ans;
         
-        while(l<=r && t<=d){
+        int r=0;
+        int c=0;
+        
+        int n=matrix.size()-1;
+        int m=matrix[0].size()-1;
+        
+        while(r<=n && c<=m){
             
-            // cout<<l<<" "<<r<<" "<<t<<" "<<d<<endl;
-            for(int i=l;i<=r;i++)
-                v.push_back(matrix[t][i]);
-            
-              t++;
-            
-                       // cout<<l<<" "<<r<<" "<<t<<" "<<d<<endl;
-            
-           for(int i=t;i<=d;i++)
-                v.push_back(matrix[i][r]);
-            
-            r--;
-            
-                       // cout<<l<<" "<<r<<" "<<t<<" "<<d<<endl;
-            
-            if(d>=t){
-                for(int i=r;i>=l;i--)
-                v.push_back(matrix[d][i]);
-             d--;   
-            }
-             
-                       // co//ut<<l<<" "<<r<<" "<<t<<" "<<d<<endl;
-            if(r>=l){
-                for(int i=d;i>=t;i--)
-                v.push_back(matrix[i][l]);
-            l++;
-                 
-            }
-                       //cout<<l<<" "<<r<<" "<<t<<" "<<d<<endl;
+        for(int i=r;i<=m;i++)
+            ans.push_back(matrix[r][i]);
+        
+        r++;
+        
+        for(int i=r;i<=n;i++)
+            ans.push_back(matrix[i][m]);
+        
+        m--;
+        
+         if(n>=r){   
+        for(int i=m;i>=c;i--)
+            ans.push_back(matrix[n][i]);
+        // c++;
+        n--;
         }
+            
+            if(m>=c){
+        for(int i=n;i>=r;i--)
+            ans.push_back(matrix[i][c]);
         
-        return v;
+        c++;
+            }
+    }
+    
+    return ans;
     }
 };
